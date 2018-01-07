0d4b0a8a6624e5cbba33f8978b96b304dbb38776
isavita <isavitaisa@gmail.com>
2018-01-07 21:00:49 +0000

    Initialize Blockchain Exmplorer

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Blockchain from './components/blockchain.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Blockchain />
+      </div>
+    );
+  }
+}
+
+export default App;
